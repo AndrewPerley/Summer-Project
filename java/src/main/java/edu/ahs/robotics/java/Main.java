16f2cf08bb80217adfcb98b8aba6a009bb6a1a45
AndrewPerley <67243999+AndrewPerley@users.noreply.github.com>
2020-06-22 16:58:32 -0600

    did a lot

--- java/src/main/java/edu/ahs/robotics/java/Main.java
+++ java/src/main/java/edu/ahs/robotics/java/Main.java
@@ -1,7 +1,19 @@
 package edu.ahs.robotics.java;
 
+import java.sql.SQLOutput;
+import java.util.Enumeration;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!a My name is Andrew!");
+    public static void main(String[] args){
+
+        Point a=new Point(3,4);
+
+        System.out.println(a.distanceFromOrigin());
+
+        System.out.println(a.getX());
+
+        System.out.println(a);
     }
 }
+
+
