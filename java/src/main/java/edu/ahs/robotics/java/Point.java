b52a08050d7de9c944b25576aacec63b4d332ab5
AndrewPerley <67243999+AndrewPerley@users.noreply.github.com>
2020-06-25 17:00:01 -0600

    Commit of problem set two

--- java/src/main/java/edu/ahs/robotics/java/Point.java
+++ java/src/main/java/edu/ahs/robotics/java/Point.java
@@ -53,4 +53,28 @@ public class Point {
 
         }
     }
+
+    public double distanceToPoint(Point other){
+        double deltaX = x - other.getX();
+        double deltaY = y - other.getY();
+        double distance = Math.sqrt(deltaX*deltaX+deltaY*deltaY);
+        return distance;
+    }
+
+
+    public Point closestPoint(Point[]points){
+       Point closestPoint = points[0];
+
+        for (int i = 1; i <points.length ; i++) {
+          double distance = this.distanceToPoint(points[i]);
+          double distanceToClosestPoint = this.distanceToPoint(closestPoint);
+
+          if(distance<distanceToClosestPoint){
+              closestPoint = points[i];
+          }
+        }
+
+        return closestPoint;
+    }
 }
+
--- java/src/test/java/edu/ahs/robotics/java/PointTest.java
+++ java/src/test/java/edu/ahs/robotics/java/PointTest.java
@@ -1,5 +1,7 @@
 package edu.ahs.robotics.java;
 
+import com.sun.org.apache.xerces.internal.xinclude.XPointerElementHandler;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -20,5 +22,19 @@ public class PointTest {
         assertEquals("axis",new Point(0,0).getQuadrant());
         assertEquals("axis",new Point(0,-7).getQuadrant());
         assertEquals("axis",new Point(12,0).getQuadrant());
+    }
+
+    @Test
+    public void distanceToPoint() {
+      Point a = new Point(12,16);
+      Point b = new Point(9,12);
+        assertEquals(5, a.distanceToPoint(b),.000001);
+    }
+
+    @Test
+    public void closestPoint() {
+        Point a = new Point (6,10);
+        Point[] points = {new Point(5,7),new Point(3,8),new Point(6,10),new Point(-2,7)};
+        assertEquals(points[2],a.closestPoint(points));
     }
 }
